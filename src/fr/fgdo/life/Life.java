8cd20c02f8f378c48635ff235655cb7fcaf3c63b
Olivier Baurain <Olivier Baurain>
2017-03-21 00:33:20 +0100

    Remove Useless Imports

--- src/fr/fgdo/life/Life.java
+++ src/fr/fgdo/life/Life.java
@@ -13,10 +13,7 @@ import java.awt.Dimension;
 import java.awt.HeadlessException;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
 /**
