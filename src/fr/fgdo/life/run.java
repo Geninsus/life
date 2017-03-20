b65d4991027c2eda9e2038131fac69a9cd691757
Olivier Baurain <Olivier Baurain>
2017-03-21 00:32:18 +0100

    Create Alt Files

---
+++ src/fr/fgdo/life/run.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.fgdo.life;
+
+/**
+ *
+ * @author Olivier
+ */
+public class run {
+    public static void main(String[] args) {
+        System.out.println("fr.fgdo.life.run.main()");
+        Life lifeGame = new Life();
+    }
+}
