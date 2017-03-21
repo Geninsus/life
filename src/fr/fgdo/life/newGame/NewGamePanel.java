e2f2241fef4dd272549f38ec5411ff000b06284a
Olivier Baurain <Olivier Baurain>
2017-03-21 11:32:13 +0100

    Add NewGamePanel Buttons

--- src/fr/fgdo/life/newGame/NewGamePanel.java
+++ src/fr/fgdo/life/newGame/NewGamePanel.java
@@ -5,6 +5,9 @@
  */
 package fr.fgdo.life.newGame;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -16,7 +19,24 @@ public class NewGamePanel extends JPanel{
 
     public NewGamePanel() {
         super();
-        this.add(new JButton("FFFD"));
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(3, 3, 3, 3);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        add(new JButton("1"),gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        add(new JButton("2"),gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 2;
+        add(new JButton("3"),gbc);
     }
     
 }
