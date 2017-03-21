ac3fdafdb5b4f7703989aa8eb87841c7a6359b66
Olivier Baurain <Olivier Baurain>
2017-03-21 12:06:50 +0100

    Add FormatedTextField

--- src/fr/fgdo/life/newGame/NewGamePanel.java
+++ src/fr/fgdo/life/newGame/NewGamePanel.java
@@ -8,8 +8,14 @@ package fr.fgdo.life.newGame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.text.Format;
+import java.text.NumberFormat;
 import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
 
 /**
  *
@@ -28,13 +34,17 @@ public class NewGamePanel extends JPanel{
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 1;
-        add(new JButton("1"),gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        gbc.gridwidth = 1;
-        add(new JButton("2"),gbc);
+        add(new JLabel("Map Width : "),gbc);
         gbc.gridx = 0;
         gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        NumberFormat numF = NumberFormat.getNumberInstance(); 
+        numF.setMaximumIntegerDigits(13);
+        numF.setMinimumIntegerDigits(3);
+        JFormattedTextField THE_FIELD = new JFormattedTextField(numF);
+        add(THE_FIELD,gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
         gbc.gridwidth = 2;
         add(new JButton("3"),gbc);
     }
