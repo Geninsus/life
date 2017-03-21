dd1363d0e64323adca7489b8103bcd4aeae0648e
Olivier Baurain <Olivier Baurain>
2017-03-21 20:17:13 +0100

    SetUp layout

--- src/fr/fgdo/life/newGame/NewGamePanel.java
+++ src/fr/fgdo/life/newGame/NewGamePanel.java
@@ -5,6 +5,7 @@
  */
 package fr.fgdo.life.newGame;
 
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -31,22 +32,42 @@ public class NewGamePanel extends JPanel{
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new Insets(3, 3, 3, 3);
         
+        NumberFormat numF = NumberFormat.getNumberInstance(); 
+        numF.setMaximumIntegerDigits(4);
+        
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 1;
         add(new JLabel("Map Width : "),gbc);
+        
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        JFormattedTextField mapWidthTextField = new JFormattedTextField(numF);
+        mapWidthTextField.setPreferredSize(new Dimension(50, 20));
+        mapWidthTextField.setMinimumSize(new Dimension(50, 20));
+        add(mapWidthTextField,gbc);
+        
+        
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.gridwidth = 1;
-        NumberFormat numF = NumberFormat.getNumberInstance(); 
-        numF.setMaximumIntegerDigits(13);
-        numF.setMinimumIntegerDigits(3);
-        JFormattedTextField THE_FIELD = new JFormattedTextField(numF);
-        add(THE_FIELD,gbc);
+        add(new JLabel("Map Height : "),gbc);
+        
+        
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        JFormattedTextField mapHeightTextField = new JFormattedTextField(numF);
+        mapHeightTextField.setPreferredSize(new Dimension(50, 20));
+        mapHeightTextField.setMinimumSize(new Dimension(50, 20));
+        add(mapHeightTextField,gbc);
+        
         gbc.gridx = 0;
         gbc.gridy = 2;
         gbc.gridwidth = 2;
-        add(new JButton("3"),gbc);
+        JButton confirmButton = new JButton("Confirm");
+        add(confirmButton,gbc);
     }
     
 }
